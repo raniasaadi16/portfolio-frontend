258be5b1508e6870eed0ee44550ef8c4e2d8c51d
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2021-10-15 23:13:33 +0200

    fix project gallery background

--- components/portfolio/Project.jsx
+++ components/portfolio/Project.jsx
@@ -13,10 +13,10 @@ export default function Project({projectId}) {
     if(!data) return <p>Loading...</p>
     return (
         <div className='flex flex-wrap w-full flex-col-reverse md:flex-row md:space-x-5'>
-            <div className='bg-white py-5 px-2 md:w-8/12 w-full rounded-sm'>
+            <div className='py-5 px-2 md:w-8/12 w-full rounded-sm' style={{background: '#f9f9f9'}}>
                 <Gallery pictures={data.gallery} alt={data.projectName}/>
             </div>
-            <div className='py-3 px-4 rounded-sm mb-5 md:mb-0 border-t-8 border-pink-light md:w-[30%] md:h-[max-content]' style={{background: '#f9f9f9'}}>
+            <div className='bg-white py-3 px-4 rounded-sm mb-5 md:mb-0 border-t-8 border-pink-light md:w-[30%] md:h-[max-content]'>
                 <Description project={data}/>
             </div>
         </div>
